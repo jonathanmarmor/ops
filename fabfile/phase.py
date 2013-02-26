b2d138235978c4345a38a2487774c430a9b2eade
Jonathan Marmor <jmarmor@gmail.com>
2013-02-26 00:50:00 -0500

    Add fabfile for phase app.

---
+++ fabfile/phase.py
@@ -0,0 +1,42 @@
+from fabric.api import env, task
+
+import apt
+import git
+import npm
+import forever
+
+
+env.repo = 'phase'
+
+
+@task
+def install():
+    apt.install()
+    git.clone(env.repo)
+    npm.install(env.repo)
+    npm.install_globals('forever')
+    start()
+
+
+@task
+def start():
+    forever.start(env.repo)
+
+
+@task
+def stop():
+    forever.stop(env.repo)
+
+
+@task
+def restart():
+    forever.restart(env.repo)
+
+
+@task
+def deploy(rm_node_modules=False):
+    git.update(env.repo)
+    if rm_node_modules:
+        npm.rm_node_modules(env.repo)
+    npm.install(env.repo)
+    restart()
