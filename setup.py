95cf36a9705594cf8fffd9351e35339c593e6b47
Jonathan Marmor <jm@ex.fm>
2013-02-28 18:43:08 -0500

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ops',
     version='0.1',
-    description='Fabric tasks setting up and managing my apps.',
+    description='Fabric tasks for setting up and managing my apps.',
     author='Jonathan Marmor',
     author_email='jmarmor@gmail.com',
     url='http://github.com/jonathanmarmor/ops',
